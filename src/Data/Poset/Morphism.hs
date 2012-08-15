ec139c6aea83d9e2e0af89651c957e8f2feb7616
Dmitry Malikov <color404051h@gmail.com>
2012-08-15 20:42:19 +0400

    Add morphism constructors, add test for morphism.

--- src/Data/Poset/Morphism.hs
+++ src/Data/Poset/Morphism.hs
@@ -14,8 +14,23 @@ type Morph = IM.IntMap Int
 
 -- | Build Morph from list
 --
-morph ∷ [(Int,Int)] → Morph
-morph = IM.fromList
+fromList ∷ [(Int,Int)] → Morph
+fromList = IM.fromList
+
+-- | Build Morph from permutation
+--
+fromPerm ∷ [Int] → Morph
+fromPerm = fromList . zip [1..]
+
+-- | Show Morph as a list
+--
+toList ∷ Morph → [(Int,Int)]
+toList = IM.toList
+
+-- | Show Morph as a permutation
+--
+toPerm ∷ Morph → [Int]
+toPerm = map snd . toList
 
 -- | Apply Morph to Poset element
 --
@@ -27,49 +42,49 @@ m >> a = m IM.! a
 -- | Check for isotone property
 -- a ρ b ⇒ Morph(a) ρ Morph(b)
 --
-isotone ∷ Morph → Poset → Bool
-isotone m p@(Poset es _) = and
-  [ (m >> a) `ρ` (m >> b) | a ← es, b ← es ]
+isotone ∷ Poset → Morph → Bool
+isotone p@(Poset es _) m = and
+  [ (m >> a) `ρ` (m >> b) | a ← es, b ← es, a `ρ` b ]
     where ρ = binaryRelation p
 
 -- | Check for reducibility
 -- Morph(a) ρ a
 --
-reducible ∷ Morph → Poset → Bool
-reducible m p@(Poset es _) = and
+reducible ∷ Poset → Morph → Bool
+reducible p@(Poset es _) m = and
   [ (m >> a) `ρ` a | a ← es ]
     where ρ = binaryRelation p
 
 -- | Check for idempotency
 -- Morph ∘ Morph ≡ Morph
 --
-idempotent ∷ Morph → Poset → Bool
-idempotent m (Poset es _) = and
+idempotent ∷ Poset → Morph → Bool
+idempotent (Poset es _) m = and
   [ (m >> m >> a) == (m >> a) | a ← es ]
 
 -- | Check for fixity
 -- b ≡ Morph(b) AND a ρ b ⇒ a ≡ Morph(a)
 --
-fixed ∷ Morph → Poset → Bool
-fixed m p@(Poset es _) = and
+fixed ∷ Poset → Morph → Bool
+fixed p@(Poset es _) m = and
   [ a == (m >> a) | b ← es, b == (m >> b), a ← es, a `ρ` b ]
     where ρ = binaryRelation p
 
 -- | Check for:
--- - isotone property
--- - reducibility
--- - idempotency
--- - fixity
+-- - isotone property;
+-- - reducibility;
+-- - idempotency;
+-- - fixity;
 --
-isValid ∷ Morph → Poset → Bool
-isValid m p = and
-  [ isotone m p
-  , reducible m p
-  , idempotent m p
-  , fixed m p
+nice ∷ Poset → Morph → Bool
+nice p m = and
+  [ isotone p m
+  , reducible p m
+  , idempotent p m
+  , fixed p m
   ]
 
 -- | Generate all possible Morphs over n-size set
 --
 generate ∷ Int → [Morph]
-generate n = map (morph . zip [1..]) $ replicateM n [1..n]
+generate n = map fromPerm $ replicateM n [1..n]
